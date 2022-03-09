fb091f8765ce4467d1bcfcdc961f5f0a32a07635
Edison Padilla <edisojp@gmail.com>
2022-03-09 19:51:28 -0400

    fix/ add where condition and category by slug

--- src/modules/category/controller.ts
+++ src/modules/category/controller.ts
@@ -1,46 +1,44 @@
-import * as cacheKeys from "./cache-keys";
+import * as cacheKeys from './cache-keys';
 
-import { createQueryBuilder } from "typeorm";
-import { Category } from "../../entity/category";
+import { createQueryBuilder } from 'typeorm';
+import { Category } from '../../entity/category';
 
-// *** Queries *** //
-async function categories(cache) {
+const categories = async (cache: any) => {
   const cacheCategories = await cache.get(cacheKeys.categories);
   if (!!cacheCategories) return JSON.parse(cacheCategories);
 
-  const categories = await createQueryBuilder<Category>(Category, "category")
-    .select(["category.id", "category.name", "category.slug"])
-    .getMany();
+  const categories = await createQueryBuilder<Category>(Category, 'category')
+      .select(['category.id', 'category.name', 'category.slug'])
+      .getMany();
 
   await cache.set(cacheKeys.categories, JSON.stringify(categories));
   return categories;
-}
+};
 
-async function categoryBySlug(slug: string, cache) {
-  const key = cacheKeys.categoryBySlug.replace(":slug", slug);
-
+const categoryBySlug = async (slug: string, cache: any) => {
+  const key = cacheKeys.categoryBySlug.replace(':slug', slug);
   const cacheCategory = await cache.get(key);
   if (!!cacheCategory) return JSON.parse(cacheCategory);
 
-  const category = await createQueryBuilder<Category>(Category, "category")
-    .select([
-      "category.id",
-      "category.name",
-      "category.slug",
-      "children.id",
-      "children.name",
-      "children.slug",
-    ])
-    .leftJoin(
-      "category.children",
-      "children",
-      "children.parentId = category.id"
-    )
-    .getOne();
+  const category = await createQueryBuilder<Category>(Category, 'category')
+      .select([
+        'category.id',
+        'category.name',
+        'category.slug',
+        'children.id',
+        'children.name',
+        'children.slug',
+      ])
+      .leftJoin(
+          'category.children',
+          'children',
+          'children.parentId = category.id',
+      ).where('category.slug = :slug', { slug })
+      .getOne();
 
   await cache.set(key, JSON.stringify(category));
   return category;
-}
+};
 
 export {
   // *** Queries *** //
